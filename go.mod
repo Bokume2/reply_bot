4932ba9cb849f555aba96f808f8579f1ad792bee
Bokume2 <nagatomoyuu2@gmail.com>
2026-03-05 10:35:56 +0900

    feat: DB初期化スクリプトを追加

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v5 v5.0.4
 	golang.org/x/net v0.51.0
+	golang.org/x/text v0.34.0
 )
 
 require (
@@ -64,7 +65,6 @@ require (
 	golang.org/x/crypto v0.48.0 // indirect
 	golang.org/x/oauth2 v0.35.0 // indirect
 	golang.org/x/sys v0.41.0 // indirect
-	golang.org/x/text v0.34.0 // indirect
 	golang.org/x/time v0.14.0 // indirect
 	google.golang.org/protobuf v1.36.11 // indirect
 	gopkg.in/neurosnap/sentences.v1 v1.0.7 // indirect
---
+++ scripts/seeds.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"reply_bot/internal/infrastructure/config"
+	"reply_bot/internal/infrastructure/storage"
+	"reply_bot/internal/interface/schema"
+	"time"
+
+	"github.com/go-ap/activitypub"
+	"golang.org/x/text/language"
+)
+
+func CreateBotActor() error {
+	actor := activitypub.ActorNew(schema.UsernameToId(config.BOT_PREFERRED_USERNAME), activitypub.ServiceType)
+	actor.Name.Set(activitypub.LangRef(language.Japanese), activitypub.Content(config.BOT_NAME))
+	actor.PreferredUsername.Set(activitypub.LangRef(language.Japanese), activitypub.Content(config.BOT_PREFERRED_USERNAME))
+	now := time.Now()
+	actor.Published = now
+	actor.StartTime = now
+	actor.Updated = now
+	activitypub.Inbox.AddTo(actor)
+	activitypub.Outbox.AddTo(actor)
+	activitypub.Following.AddTo(actor)
+	activitypub.Followers.AddTo(actor)
+	item, err := storage.DataStore.Save(actor)
+	if err != nil {
+		return err
+	}
+	b, err := activitypub.MarshalJSON(item)
+	var buf bytes.Buffer
+	err = json.Indent(&buf, b, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println()
+	fmt.Println("Created actor showed bellow:")
+	fmt.Println()
+	fmt.Println(buf.String())
+	return nil
+}
+
+func main() {
+	config.LoadEnv()
+	storage.InitStorage()
+
+	fmt.Println()
+	fmt.Println("Starting to generate seed data...")
+	fmt.Println()
+
+	if err := CreateBotActor(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println()
+	fmt.Println("Finished to initialize data.")
+}
