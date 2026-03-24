e1b73921c70f9952ee65dc1894aa55e448faf188
Bokume2 <nagatomoyuu2@gmail.com>
2026-03-24 13:15:14 +0900

    update: WebfingerのHandlerをシングルトン的な運用にし、ログレベルを変更

--- internal/interface/controller/well_known.go
+++ internal/interface/controller/well_known.go
@@ -1,11 +1,19 @@
 package controller
 
 import (
+	"net/http"
+	"sync"
+
 	"github.com/Bokume2/reply_bot/internal/infrastructure/storage"
 
 	"git.sr.ht/~mariusor/lw"
 	"github.com/go-ap/webfinger"
 	"github.com/labstack/echo/v5"
+)
+
+var (
+	h       wfHandler
+	wkcOnce sync.Once
 )
 
 type WellKnownController struct{}
@@ -15,6 +23,17 @@ func NewWellKnownController() *WellKnownController {
 }
 
 func (wkc WellKnownController) GetWebfinger(c *echo.Context) error {
-	webfinger.New(lw.Prod(), storage.WebFingerStorage).HandleWebFinger(c.Response(), c.Request())
+	handler().HandleWebFinger(c.Response(), c.Request())
 	return nil
+}
+
+func handler() wfHandler {
+	wkcOnce.Do(func() {
+		h = webfinger.New(lw.Dev(), storage.WebFingerStorage)
+	})
+	return h
+}
+
+type wfHandler interface {
+	HandleWebFinger(w http.ResponseWriter, r *http.Request)
 }
