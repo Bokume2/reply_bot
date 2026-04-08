82741a608b9ee3637047b9b9dcc1b9a7bcc0ed67
Bokume2 <nagatomoyuu2@gmail.com>
2026-04-09 00:22:29 +0900

    feat: host-metaを実装

--- internal/interface/controller/well_known.go
+++ internal/interface/controller/well_known.go
@@ -1,9 +1,11 @@
 package controller
 
 import (
+	"encoding/xml"
 	"net/http"
 	"sync"
 
+	"github.com/Bokume2/reply_bot/internal/infrastructure/config"
 	"github.com/Bokume2/reply_bot/internal/infrastructure/storage"
 
 	"git.sr.ht/~mariusor/lw"
@@ -36,4 +38,32 @@ func handler() wfHandler {
 
 type wfHandler interface {
 	HandleWebFinger(w http.ResponseWriter, r *http.Request)
+}
+
+func (wkc WellKnownController) GetHostMeta(c *echo.Context) error {
+	content, err := xml.MarshalIndent(HostMetaData{
+		Xmlns: "http://docs.oasis-open.org/ns/xri/xrd-1.0",
+		Link: HostMetaLink{
+			Rel:      "lrdd",
+			Type:     "application/xrd+xml",
+			Template: config.LocalOrigin().String() + webfinger.WellKnownWebFingerPath + "?resource={uri}",
+		},
+	}, "", "  ")
+	if err != nil {
+		return err
+	}
+	c.Response().Header().Set(echo.HeaderContentType, "application/xrd+xml")
+	return c.String(http.StatusOK, xml.Header+string(content))
+}
+
+type HostMetaData struct {
+	XMLName xml.Name     `xml:"XRD"`
+	Xmlns   string       `xml:"xmlns,attr"`
+	Link    HostMetaLink `xml:"Link"`
+}
+
+type HostMetaLink struct {
+	Rel      string `xml:"rel,attr"`
+	Type     string `xml:"type,attr"`
+	Template string `xml:"template,attr"`
 }
