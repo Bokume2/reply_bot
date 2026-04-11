aa921590d2bc3b1ca83100b911e98c294a50d00f
Bokume2 <nagatomoyuu2@gmail.com>
2026-04-11 16:26:11 +0900

    refactor: デフォルトの@context定義を公開

--- pkg/jsonld/jsonld.go
+++ pkg/jsonld/jsonld.go
@@ -1,17 +1,37 @@
 package jsonld
 
 import (
-	"github.com/go-ap/activitypub"
 	"github.com/go-ap/jsonld"
 	"github.com/labstack/echo/v5"
 )
 
+const contextKey = "@context"
+
+var defaultContext = map[string]any{
+	contextKey: []any{
+		"https://www.w3.org/ns/activitystreams",
+		"https://w3id.org/security/v1",
+	},
+}
+
+func DefautContext() map[string]any {
+	return defaultContext
+}
+
+func defautContextGoAP() []jsonld.Collapsible {
+	var ctx []jsonld.Collapsible
+	for _, e := range DefautContext()[contextKey].([]any) {
+		switch v := e.(type) {
+		case string:
+			ctx = append(ctx, jsonld.IRI(v))
+		}
+	}
+	return ctx
+}
+
 func JSONLDMarshal(obj any, ctx ...jsonld.Collapsible) ([]byte, error) {
 	if len(ctx) == 0 {
-		ctx = []jsonld.Collapsible{
-			jsonld.IRI(activitypub.ActivityBaseURI),
-			jsonld.IRI("https://w3id.org/security/v1"),
-		}
+		ctx = defautContextGoAP()
 	}
 	payload := jsonld.WithContext(ctx...)
 	return payload.Marshal(obj)
